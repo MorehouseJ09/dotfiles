902c571ec419fc0c05798078c3767365e8cf6e36
Jon Morehouse <morehousej09@gmail.com>
2014-05-18 04:18:27 -0700

    Update structure. Set environment loading as needed.

--- src/install.sh
+++ src/install.sh
@@ -8,23 +8,32 @@ function submodules {
     fi
 
     # install linux brew if not on mac
-    if [ ]
-
+    if [ "$OS" == "linux" ]; then
+        git clone git://github.com/homebrew/linuxbrew $HOME/.linuxbrew
+    fi
 
 }
 
-function symlinks {
+function symlinks() {
     
-    # symlink 
-
-    # symlink personal 
-
+    # install all symlinks to $HOME
+    for dir in "$DOTFILES_DIR" "$PERSONAL_DIR"; do
+        for symlink in $(ls $dir/symlinks); do
+            ln -f -s $dir/symlinks/$symlink $HOME/.$symlink
+        done 
+    done
 
 }
 
 # install dependencies [homebrew, homebrew formulae, casks, python, rubygems, npm modules]
 function dependencies {
 
+    #brew bundle $DOTFILES_DIR/dependencies/Brewfile
+    #pip install -r $DOTFILES_DIR/dependencies/requirements.txt
+
+    if [ $OS == "mac" ];then
+        echo "install casks"
+    fi
 
 }
 
