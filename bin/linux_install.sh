9b68c4848ab06dbbe8c0a32a43c1be4aacfd2abe
Jon Morehouse <morehousej09@gmail.com>
2014-05-21 15:37:32 -0700

    Update tmux configuration to load linux version as well

--- bin/linux_install.sh
+++ bin/linux_install.sh
@@ -15,6 +15,11 @@ sudo apt-get update
 sudo apt-get install -y build-essential curl git ruby zsh \
     texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 
+# set up rake for homebrew to use properly
+sudo gem install rake
+sudo ln -s `which rake` /usr/bin/rake
+
+
 # set default shell
 sudo echo `which zsh` >> /etc/shells
 sudo chsh -s `which zsh`
