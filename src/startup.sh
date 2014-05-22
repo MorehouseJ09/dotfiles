69aaaa5535c0dee006757ca0439da9b34de0af79
Jon Morehouse <morehousej09@gmail.com>
2014-05-21 21:02:29 -0700

    Fix startup command to hide output

--- src/startup.sh
+++ src/startup.sh
@@ -1,4 +1,4 @@
 # startup commands to be run on every new shell or reload
 if [ `which keychain` ];then
-    eval `keychain --eval >> /tmp/null 2&> /tmp/null`
+    eval `keychain --eval 2&> /tmp/null`
 fi
