8d88a78d2c11f6592c9bb20924fd74193949aeb4
Jon Morehouse <morehousej09@gmail.com>
2014-05-25 01:03:54 -0700

    Update zshell theme

--- src/zshell-theme.zsh
+++ src/zshell-theme.zsh
@@ -1,12 +1,11 @@
 # prompt name
 function prompt_char {
 
-	if [ $UID -eq 0 ]; then echo "#"; else echo $; fi
+    if [ $UID -eq 0 ]; then echo "#"; else echo $; fi
 }
 
-
 # http://www.acm.uiuc.edu/workshops/zsh/prompt/escapes.html
-PROMPT='%(!.%{$fg_bold[black]%}.%{$fg_bold[black]%})%{$fg_bold[black]%}$(path_tail 2) $(git_prompt_info)%_$(prompt_char)%{$reset_color%} '
+PROMPT='%(!.%{$fg_bold[white]%}.%{$fg_bold[white]%})%{$fg_bold[cyan]%}$(path_tail 2) $(git_prompt_info)%_$(prompt_char)%{$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="("
 ZSH_THEME_GIT_PROMPT_SUFFIX=") "
