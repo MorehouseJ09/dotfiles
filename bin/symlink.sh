ea04336e8e335d478383f327065f92dbd19583ba
Jon Morehouse <morehousej09@gmail.com>
2014-05-25 23:34:24 -0700

    Update bin scripts

---
+++ bin/symlink.sh
@@ -0,0 +1,14 @@
+# load the installer script
+if [ -z "$DOTFILES_DIR" ]; then
+    export DOTFILES_DIR=$HOME/dotfiles
+fi
+# set personal directory
+if [ -z "$PERSONAL_DIR" ]; then
+    export PERSONAL_DIR=$HOME/.personal
+fi
+
+# load installation helpers
+source $DOTFILES_DIR/src/install.sh
+
+# re-symlink all files
+symlinks
