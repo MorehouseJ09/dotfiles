84a86086fd1448d3f08e0e5361db32c00a04938f
Jon Morehouse <morehousej09@gmail.com>
2014-05-28 17:48:30 -0700

    Update prompt and zshell-theme for new prompt which replaces home directory smartly

--- src/zshell-theme.zsh
+++ src/zshell-theme.zsh
@@ -10,5 +10,17 @@ function machine_name {
     fi
 }
 
+function prompt_path {
+    
+    current_prefix=""
+    # normalize the path to use a tilda!
+    if [[ $(pwd) = $HOME/* ]]; then
+        current_prefix="~" 
+    fi
+    
+    # grab main body
+    body=`pwd | sed 's|'$HOME'||g' | awk '{ n=split($1,a,"/")} END{ print a[n-1] "/" a[n] }'`
+    echo "${current_prefix}${body}"
+}
+
 ZSH_THEME_GIT_PROMPT_PREFIX="("
-ZSH_THEME_GIT_PROMPT_SUFFIX=") "
