7a7c9575f719f30b8c9688a7b4cdabc13f4b5b1a
Jon Morehouse <morehousej09@gmail.com>
2014-06-12 16:26:54 -0700

    Update chromebook element.

--- bin/chromebook_ssh.sh
+++ bin/chromebook_ssh.sh
@@ -1,6 +1,5 @@
 sudo apt-get install -y openssh-server openssh-client iptables
 
-sudo sh -c 'echo "mkdir -p -m0755 /var/run/sshd" >> /etc/rc.local'
-sudo sh -c 'echo "/usr/sbin/sshd" >> /etc/rc.local'
-sudo sh -c 'echo "/sbin/iptables -I INPUT -p tcp --dport 22 -j ACCEPT" >> /etc/rc.local'
-
+sudo sh -c 'mkdir -p -m0755 /var/run/sshd'
+sudo sh -c '/sbin/iptables -I INPUT -p tcp --dport 22 -j ACCEPT'
+sudo sh -c '/usr/sbin/sshd'
