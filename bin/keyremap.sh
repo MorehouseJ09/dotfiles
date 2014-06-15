4f8eec8a9867599dc810afa7de317ae619143ce5
Jon Morehouse <morehousej09@gmail.com>
2014-06-15 01:39:08 -0700

    Update system. Get better keyboard hacks working

---
+++ bin/keyremap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cli=/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli
+
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+/bin/echo
