46e0df21b0b6a3020072133e49b01a4ec76accf3
Jon Morehouse <morehousej09@gmail.com>
2014-08-20 13:18:21 -0700

    Fix project_root functions

--- src/shell-functions.sh
+++ src/shell-functions.sh
@@ -1,3 +1,15 @@
+function project_root {
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+    echo "$(git rev-parse --show-toplevel)"
+  else
+    echo "$(pwd)"
+  fi
+}
+
+function in_project_root {
+  cd "$(project_root)"
+}
+
 function remove_function {
 
     type $1 | grep -q 'shell function'
@@ -7,52 +19,52 @@ function remove_function {
 # initialize a path segment -- grab relative path
 function path_tail {
 
-	echo `pwd` | awk '{ n=split($1,a,"/")} END{ print a[n-1] "/" a[n] }'
+  echo `pwd` | awk '{ n=split($1,a,"/")} END{ print a[n-1] "/" a[n] }'
 }
 
 # get the absolute path of a file
 function path {
 
-	if [[ ${1} == "." || ${1} == ".." ]]; then
-		_path="`pwd`"
+  if [[ ${1} == "." || ${1} == ".." ]]; then
+    _path="`pwd`"
 
-	else
-		_path="`pwd`/$1"
-	fi
+  else
+    _path="`pwd`/$1"
+  fi
 
-	echo "${_path}"
+  echo "${_path}"
 }
 
 
 # create a function that is useful for loading in custom configuration for local projects
 function custom_shell {
 
-	# now see if the file exists
-	if [[ -f ./.shell_config ]]; then
-		#statements
-		source .shell_config
-		echo "Source Loaded."
-	fi
-	
+  # now see if the file exists
+  if [[ -f ./.shell_config ]]; then
+    #statements
+    source .shell_config
+    echo "Source Loaded."
+  fi
+  
 }
 
 function reload {
-	
-	# check to see if current shell is zsh or not so we can load proper main files
-	if [[ $SHELL == */zsh ]];then
+  
+  # check to see if current shell is zsh or not so we can load proper main files
+  if [[ $SHELL == */zsh ]];then
 
-		source $DOTFILES_DIR/bin/bootstrap.sh
-	else
-		
-		source $DOTFILES_DIR/bin/bootstrap.sh		
-	fi
-	
-	# load up any .shell_configs
-	custom_shell
+    source $DOTFILES_DIR/bin/bootstrap.sh
+  else
+    
+    source $DOTFILES_DIR/bin/bootstrap.sh   
+  fi
+  
+  # load up any .shell_configs
+  custom_shell
 
-	# check to see if we are currently running a  virtualenv in python
-	if [[ -d bin && -f bin/activate ]];then
-		source bin/activate
-	fi
+  # check to see if we are currently running a  virtualenv in python
+  if [[ -d bin && -f bin/activate ]];then
+    source bin/activate
+  fi
 }
 
