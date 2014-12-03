8ac51ccd34f0272196d78995c015d9b03140f274
Jon Morehouse <morehousej09@gmail.com>
2014-12-03 09:10:18 -0800

    Fix double indentation

--- src/startup.sh
+++ src/startup.sh
@@ -1,26 +1,25 @@
 # startup commands to be run on every new shell or reload
 if (( $+commands[keychain] )); then
+  # start a new gpg-agent for this process
+  eval `keychain --eval -q`
+  export GPG_PID=`echo $GPG_AGENT_INFO | awk '{ split($0,a,":"); print a[2] }'`
+  
+  # handle gpg_processes
+  gpg_file="/tmp/gpg_agents"
 
-    # start a new gpg-agent for this process
-    eval `keychain --eval -q`
-    export GPG_PID=`echo $GPG_AGENT_INFO | awk '{ split($0,a,":"); print a[2] }'`
-    
-    # handle gpg_processes
-    gpg_file="/tmp/gpg_agents"
+  # now store this current tty/pid in /tmp/gpg_agents
+  echo "$(tty) $GPG_PID" >> $gpg_file
 
-    # now store this current tty/pid in /tmp/gpg_agents
-    echo "$(tty) $GPG_PID" >> $gpg_file
+  # export GPG_TTY so that this tty is used here locally
+  export GPG_TTY=$(tty)
 
-    # export GPG_TTY so that this tty is used here locally
-    export GPG_TTY=$(tty)
+  # update pinentry tty to use current tty
+  echo UPDATESTARTUPTTY | gpg-connect-agent >> /dev/null
 
-    # update pinentry tty to use current tty
-    echo UPDATESTARTUPTTY | gpg-connect-agent >> /dev/null
-
-    # run gpg-bootstrap if we have a .passphrase file
-    if [[ -f $HOME/.passphrase ]]; then 
-        $DOTFILES_DIR/scripts/gpg-bootstrap 2>&1 > /dev/null
-    fi
+  # run gpg-bootstrap if we have a .passphrase file
+  if [[ -f $HOME/.passphrase ]]; then 
+      $DOTFILES_DIR/scripts/gpg-bootstrap 2>&1 > /dev/null
+  fi
 fi
 
 if (( $+commands[docker-osx] )); then
